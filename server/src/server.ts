9f18d35218a64343ce415bc0cbcca7a1bec685c6
AnisJshK <anisshaikh2131@gmail.com>
2026-05-21 11:05:25 +0530

    made-changes-in-server

--- server/src/server.ts
+++ server/src/server.ts
@@ -20,7 +20,7 @@ app.use(clerkMiddleware())
 app.get("/",async(req,res)=>{
     res.send('Server is Live!')
 })
-app.use('/api/inngest',serve({client:inngest,functions}))
+app.use('/api/inngest',serve({client:inngest,functions:[]}))
 
  
 app.listen(PORT,()=>{
